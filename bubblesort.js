c611701ffb322456f9d3d558bccb299984f7710f
Russell Moore <russellwmoore@gmail.com>
2018-09-17 12:26:36 -0400

    Init Commit

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+function bubbleSort(array) {
+  let end = array.length;
+  let pointer = arr[0];
+
+  for(let i = 0; i < end; i++) {
+
+  }
+
+}
+
+
+function swap(indexA, indexB, arr) {
+  let first = arr[indexA];
+  let second = arr[indexB];
+
+  arr[indexA] = second;
+  arr[indexB] = first;
+
+  return arr;
+}
